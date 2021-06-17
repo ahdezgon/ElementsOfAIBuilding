e3acd2dc4f02ba402c9998fb067f516334eea9f1
Adrián Hernández González <80392474+ahdezgon@users.noreply.github.com>
2021-06-17 22:41:32 +0200

    Create Ejercicio4.py

---
+++ Ejercicio4.py
@@ -0,0 +1,15 @@
+import random
+def main():
+    favorite = "none"
+    prob = random.random()
+    if prob < 0.2:
+        favorite = "cats"
+        if prob < 0.1:
+            favorite = "bats"
+    else:
+        favorite = "dogs"
+
+    print("I love " + favorite)
+
+
+main()
